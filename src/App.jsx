eeede8d7819e8d34e499f64362e7c8fca556f078
Vansh Vasishtha <vansh19092002@gmail.com>
2024-08-02 21:10:25 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl text-red-500 font-bold underline">
+      <div className="flex">
+        <div className="flex-none w-14 ...">01</div>
+        <div className="flex-initial w-64 ...">02</div>
+        <div className="flex-initial w-32 ...">03</div>
+      </div>
+    </h1>
+  );
+}
